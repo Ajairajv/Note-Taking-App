283f6fa994b17344bd654fc2669a655d7e7082f1
Ajai <armmjvs@gmail.com>
2025-06-15 04:27:14 +0530

    controller added

--- backend/controllers/notesController.js
+++
@@ -1,3 +0,0 @@
-(req,res)=>{
- res.status(200).send("you got 200 notes");
-}
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,9 @@ import notesRoutes from "./routes/notesRoutes.js";
 const app = express();
 
 app.use("/api/notes",notesRoutes);
+// app.use("/api/notes",notesRoutes);
+// app.use("/api/notes",notesRoutes);
+// app.use("/api/notes",notesRoutes);
 
 // what is endpoint?
 // an endpoint is a specific URL where an API can be accessed by a client application
